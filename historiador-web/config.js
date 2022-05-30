f431e17e1e1193db19d90c0a6d4703a7c6731058
Christopher Castro <63797614+Christopher-Castro@users.noreply.github.com>
2022-05-29 22:31:33 -0500

    hotfix remote front access

--- historiador-web/config.js
+++ historiador-web/config.js
@@ -1,8 +1,10 @@
 'use strict'
+var os = require("os");
+var hostname = os.hostname();
 
 module.exports = {
   endpoint: process.env.API_ENDPOINT || 'http://localhost:3000',
-  serverHost: process.env.SERVER_HOST || 'http://localhost:8080',
+  serverHost: process.env.SERVER_HOST || `http://${hostname}:8080`,
   // apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiYWRtaW4iOnRydWUsInBlcm1pc3Npb25zIjpbIm1ldHJpY3M6cmVhZCJdLCJpYXQiOjE2MDkxODk4ODV9.J_OHmSIPpC62dEJ3_p2GhwGWXGE8oOcjdEWRaiVQMD8'
   apiToken: process.env.API_TOKEN || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN1cGVyQWRtaW4iLCJzdXBlckFkbWluIjp0cnVlLCJwZXJtaXNzaW9ucyI6WyJtZXRyaWNzOnJlYWQiXSwiaWF0IjoxNjUyNTQzMTk0fQ.zrp2mYzADnIDZWnYT1jCLUKbvL8E_7b5DAb4BRsoue0'
 }
